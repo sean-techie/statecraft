7eea5de3a8930ccba5a3df831bb67967251986d8
Reseanne Rampou <reseanner@gmail.com>
2026-07-03 00:20:32 +0200

    StateCraft lesson 1 to 4: variables and first useState

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,34 @@
+"use client";
+
+import {useState} from "react";
+
 export default function Home() {
+
+  const title="StateCraft Learning Journey";
+  const [lesson,setLesson]=useState(4) ;
+  const topic="Variables and JSX";
+  
   return (
-    <main className="flex items-center justify-center min-h-screen">
-      <h1 className="text-4xl font-bold">
-        StateCraft Learning Journey
+    <main className="min-h-screen bg-pink-700 text-white flex flex-col items-center justify-center">
+      <h1 className="text-5xl font-bold text-white-400">
+        {title}
       </h1>
+
+      <p>
+        Lesson {lesson}
+      </p>
+
+      <p>
+        Topic: {topic}
+      </p>
+
+      <button onClick={() =>setLesson(lesson + 1)}>
+      Next Lesson
+      </button>
     </main>
   );
 }
+
 
 
 
